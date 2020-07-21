b885634b58917cc724f6a2e6c928e5baa5719d58
PetoriousBIG <peter.m.martin@maine.edu>
2020-07-21 14:11:50 -0400

    Pairs.py

---
+++ EmailedProblems/Pairs.py
@@ -0,0 +1,35 @@
+# Problem Descirption:
+# 
+# cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
+# For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+# Given this implementation of cons: 
+# 
+# NOTE: I needed to look up this solution as I have very little experience with funtional programming and 
+#       didn't recognize it as a functional programming problem.
+# #
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+
+    return pair
+
+# Implement car and cdr.
+
+def car(f):
+    def left(a, b):
+        return a
+    return f(left)
+
+def cdr(f):
+    def right(a, b):
+        return b
+    return f(right)
+
+def main():
+    print(car(cons(3,4)))
+    print(cdr(cons(3,4)))
+    
+
+main()
+
